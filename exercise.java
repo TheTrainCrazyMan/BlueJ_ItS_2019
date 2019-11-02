c4aeb07eae2fff8ab2e7a2b72341e87e649b3cd9
TheTrainCrazyMan <thatt196@gmail.com>
2019-11-02 11:56:41 -0400

    Create exercise.java
    Add to exercises on monday

---
+++ exercise.java
@@ -0,0 +1,61 @@
+//Exercise 6
+System.out.println("System.out.println("Hello World!");");
+
+//Exercise 7
+System.out.println("There's one thing every coder must understand:");
+System.out.println("The System.out.println command.");
+System.out.println("\n");
+System.out.println("There's one thing every coder must understand:");
+System.out.println("The System.out.println command.");
+
+//Exercise 8
+System.out.println("//////////////////////");
+System.out.println("|| Victory is mine! ||");
+System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+System.out.println("|| Victory is mine! ||");
+System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+System.out.println("|| Victory is mine! ||");
+System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+System.out.println("|| Victory is mine! ||");
+System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+System.out.println("|| Victory is mine! ||");
+System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+//Exercise 9
+System.out.println("  ________   ");
+System.out.println(" /        \\ ");
+System.out.println("/          \\");
+System.out.println("-\"-\"-\"-\"-");
+System.out.println("\\          /");
+System.out.println(" \\        / ");
+System.out.println("  _________  ");
+
+//Exercise 10
+System.out.println("  ________   ");
+System.out.println(" /        \\ ");
+System.out.println("/          \\");
+System.out.println("\\          /");
+System.out.println(" \\        / ");
+System.out.println("  _________  ");
+System.out.println("-\"-\"-\"-\"-");
+System.out.println("  ________   ");
+System.out.println(" /        \\ ");
+System.out.println("/          \\");
+System.out.println("\\          /");
+System.out.println(" \\        / ");
+System.out.println("  _________  ");
+
+//Exercise 11
+System.out.println("  /\\       /\\    ")
+System.out.println(" /  \\     /  \\   ")
+System.out.println("/    \\   /    \\  ")
+System.out.println("+------+  +------+ ")
+System.out.println("|      |  |      | ")
+System.out.println("|      |  |      | ")
+System.out.println("|United|  |United| ")
+System.out.println("|States|  |States| ")
+System.out.println("|      |  |      | ")
+System.out.println("|      |  |      | ")
+System.out.println("+------+  +------+ ")
+System.out.println("   /\\       /\\   ")
+System.out.println("  /  \\     /  \\  ")
