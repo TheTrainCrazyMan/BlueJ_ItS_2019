2d09baaf489588cedef88bb3346882b8ef4b6657
TheTrainCrazyMan <thatt196@gmail.com>
2019-11-20 17:16:04 -0500

    Create exercise_16.java

---
+++ exercise_16.java
@@ -0,0 +1,41 @@
+//Exercise 16
+Dull1();
+
+public static void Dull1()
+{
+  Dull2();
+  Dull2();
+  Dull2();
+  Dull2();
+  Dull2();
+}
+
+public static void Dull2()
+{
+  Dull3();
+  Dull3();
+  Dull3();
+  Dull3();
+  Dull3();
+}
+
+public static void Dull3()
+{
+  Dull4();
+  Dull4();
+  Dull4();
+  Dull4();
+  Dull4();
+}
+
+public static void Dull4()
+{
+  System.out.println("All work and no play makes Jack a dull boy.");
+  System.out.println("All work and no play makes Jack a dull boy.");
+  System.out.println("All work and no play makes Jack a dull boy.");
+  System.out.println("All work and no play makes Jack a dull boy.");
+  System.out.println("All work and no play makes Jack a dull boy.");
+  System.out.println("All work and no play makes Jack a dull boy.");
+  System.out.println("All work and no play makes Jack a dull boy.");
+  System.out.println("All work and no play makes Jack a dull boy.");
+}
