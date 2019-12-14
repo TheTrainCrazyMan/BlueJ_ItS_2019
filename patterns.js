5663e9032e064c1e4ba4e44b08dddaf63599e2ab
TheTrainCrazyMan <thatt196@gmail.com>
2019-12-14 12:37:14 -0500

    Create patterns.js

---
+++ patterns.js
@@ -0,0 +1,9 @@
+//----Pattern 1----
+//--1,10,100,1000--
+for (int i = 1, n = 1; n<=7; n++)
+  System.out.print(i + ",");
+  i = i * 10
+//----Pattern 2----
+//--10,21,33,46,60--
+for (int i = 0, n = 1, k = 10; n<=7; n++)
+  System.out.print(i + ",")
