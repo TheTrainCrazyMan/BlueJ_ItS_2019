a6dead74a4b25dd308f29bb3d53c0bd596f43e78
TheTrainCrazyMan <thatt196@gmail.com>
2019-12-28 13:33:35 -0500

    Create exercises_1_to_23.java

---
+++ exercises_1_to_23.java
@@ -0,0 +1,140 @@
+//Exercise 1
+char s, v, t, a;
+int answer;
+
+answer = s + (v * t) + (0.5 * (t * t));
+
+System.out.println("The answer is " + answer);
+
+//Exercise 2
+for (i = 1, j = 1; i <= 10; i++);
+{
+   j = i * i;
+   System.out.println(j);
+} 
+//Exercise 3
+for (i = 1, j = 1, k = 1; k <= 12; k ++, i++);
+{
+   j = i + i;
+
+//Exercise 4
+for (i = 1, i <= 4, i++)
+{
+   System.out.println("*****");
+}
+
+//Exercise 5
+System.out.println("*    ")
+System.out.println("**   ")
+System.out.println("***  ")
+System.out.println("**** ")
+System.out.println("*****")
+
+//Exercise 6
+System.out.println("1      ")
+System.out.println("22     ")
+System.out.println("333    ")
+System.out.println("4444   ")
+System.out.println("55555  ")
+System.out.println("666666 ")
+System.out.println("7777777")
+
+//Exercise 7
+System.out.println("    1")
+System.out.println("   2 ")
+System.out.println("  3  ")
+System.out.println(" 4   ")
+System.out.println("5    ")
+//Exercise 8
+System.out.println("    1")
+System.out.println("   22")
+System.out.println("  333")
+System.out.println(" 4444")
+System.out.println("55555")
+//Exercise 9
+
+//Exercise 10
+
+//Exercise 11
+
+//Exercise 12
+for (i = 1, i <= 4, i++)
+{
+   System.out.println("000111222333444555666777888999");
+}
+
+//Exercise 13
+for (i = 1, i <= 5, i++)
+{
+   System.out.println("999998888877777666665555544444333332222211111");
+}
+
+//Exercise 14
+for (i = 1, i <= 4, i++)
+{
+   System.out.println("999999999888888887777777666666555554444333221");
+}
+
+//Exercise 15
+
+//Exercise 16
+
+//Exercise 17
+
+//Exercise 18
+Top();
+Window();
+Top();
+Window();
+Top();
+
+public static void Top()
+{
+   System.out.println("+===+===+");
+}
+
+public static void Window()
+{
+   Line();
+   Line();
+   Line();
+}
+
+public static void Line()
+{
+   System.out.println("|   |   |");
+
+//Exercise 19
+public static void Window()
+{
+   for (int i = 3, j = 2, i >= 0, i--, j--)
+     {
+        System.out.println("+===+===+")
+        j=j-1
+        if (j>=0)
+        {
+           System.out.println("|   |   |")
+           System.out.println("|   |   |")
+           System.out.println("|   |   |")
+        }
+     }
+}
+
+//Exercise 20
+for (int i = 1, i <= 4, i++)
+{
+   
+//Exercise 21
+
+//Exercise 22
+char spacelarge = *******, spacemed = ****, space = **
+System.out.println("$$$$$$$" + spacelarge + spacelarge + "$$$$$$$")
+System.out.println(space + "$$$$$$" + spacemed + spacemed + spacemed + "$$$$$$" + space)
+System.out.println(spacemed + "$$$$$" + spacemed + spacemed + space + "$$$$$" + spacemed)
+System.out.println(spacemed + space + "$$$$" + spacemed + spacemed + "$$$$" + space + spacemed)
+System.out.println(spacelarge + space + "*$$" + spacemed + "$$*" + spacelarge + space)
+System.out.println(spacelarge + spacemed + "*$" + space + "$*" + spacemed + spacelarge)
+
+//Exercise 23
+char spaces, spacelarge = *******, spacemed = ****, space = **
+for 
